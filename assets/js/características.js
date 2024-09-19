49dd69228bd65c4f768598b3b428118e60dfe8d8
Claud143 <clasbrcstro@gmail.com>
2024-09-18 22:21:48 -0600

    pagina lista

--- assets/js/características.js
+++ assets/js/características.js
@@ -1,22 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const areas = ['Player´s House', 'Ancient Egypt-Wild West-Pirate Seas-Far Future', 'Big Wave Beach-Lost City-Jurassic Marsh', 'Dark Ages', 'Modern Day', 'Frostbite Caves'];
+    const niveles = [5, 25, 32, 20, 34, 30];
 
-const ctx = document.getElementById('myChart')
-const areas = ['Player´s House','Ancient Egytp','Wild West','Pirate Seas','Big Wave Beach','Far future','Neon Mixtape Tour','Lost City','Dark Ages','Jurassic Marsh','Modern Day','Frosbit Caves']
-const niveles = [5, 25, 25, 25, 32, 25,32,32,20,32,34,30]
-
-const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: areas,
-        datasets: [{
-            label: 'Niveles',
-            data: niveles,
-            backgroundColor: [
-              'rgba(255, 99, 132, 9.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(225, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)'
-            ],
-        }]
-    }
-})
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: areas,
+            datasets: [{
+                label: 'Niveles',
+                data: niveles,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(225, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ]
+                
+                
+            }]
+        }
+        
+        
+    });
+});
